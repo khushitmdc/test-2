c5b6007b6582f82576c45f5f337224c11b00626c
khushibedar <khushi.bedar@tmdc.io>
2026-07-31 10:50:44 +0000

    Update SQL for practice_area_workshare_data

--- cadp/model/sqls/practice_area_workshare_data.sql
+++ cadp/model/sqls/practice_area_workshare_data.sql
@@ -1,1 +1,11 @@
-SELECT * FROM icebase.builderuploads.practice_area_workshare_data
+SELECT 
+    id,
+    agency,
+    account_name,
+    opportunity_name,
+    practice_area_value,
+    practice_area_name,
+    work_share,
+    amount_share,
+    concat(id, practice_area_name) as unique_id
+FROM icebase.builderuploads.practice_area_workshare_data
